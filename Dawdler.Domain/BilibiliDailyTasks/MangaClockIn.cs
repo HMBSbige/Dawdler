3ecb9bb24f202ff0bb3ad8402dc2da5196b347c1
Bruce Wayne <admin@bige0.com>
2020-12-30 17:36:09 +0800

    Fix DI

--- Dawdler.Domain/BilibiliDailyTasks/MangaClockIn.cs
+++ Dawdler.Domain/BilibiliDailyTasks/MangaClockIn.cs
@@ -2,10 +2,12 @@ using Dawdler.BilibiliUsers;
 using JetBrains.Annotations;
 using System.Threading;
 using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
 
 namespace Dawdler.BilibiliDailyTasks
 {
 	[UsedImplicitly]
+	[ExposeServices(typeof(IBilibiliDailyTask))]
 	public class MangaClockIn : IBilibiliDailyTask
 	{
 		public BilibiliUser? User { get; set; }
