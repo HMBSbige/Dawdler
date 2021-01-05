e5bf11d9993019918e34587c9f54ec58a188289c
Bruce Wayne <admin@bige0.com>
2021-01-05 11:00:34 +0800

    Update building script and CI

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
+param([string]$rid = 'linux-musl-x64')
 $ErrorActionPreference = 'Stop'
 
 Write-Host 'dotnet SDK version'
@@ -8,8 +9,9 @@ $configuration = 'Release'
 $output_dir = "$PSScriptRoot\Dawdler\bin\$configuration"
 $proj_path = "$PSScriptRoot\Dawdler\Dawdler.csproj"
 
-function Build-App
+function New-App
 {
+    param([string]$rid)
     Write-Host 'Building'
 
     $outdir = "$output_dir\$net_tfm"
@@ -17,8 +19,8 @@ function Build-App
 
     Remove-Item $publishDir -Recurse -Force -Confirm:$false -ErrorAction Ignore
 
-    dotnet publish -c $configuration -f $net_tfm $proj_path
+    dotnet publish "$proj_path" -c $configuration -f $net_tfm -p:PublishSingleFile=true --self-contained true -p:PublishTrimmed=True -p:TrimMode=Link -r $rid
     if ($LASTEXITCODE) { exit $LASTEXITCODE }
 }
 
-Build-App
+New-App $rid
